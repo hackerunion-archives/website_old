15d03df83ebd20cdfaa6f61f43a7484aef4a0df3
Jim Stewart <james.a.stewart@gmail.com>
2012-02-29 18:10:20 -0500

    Add tests for Member routes.

---
+++ spec/routing/members_routing_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe MembersController do
+
+  describe "routing" do
+
+    it "routes to index" do
+      get("/members").should route_to("members#index")
+    end
+
+    it "routes to new" do
+      get("/members/new").should route_to("members#new")
+    end
+
+    it "routes to create" do
+      post("/members").should route_to("members#create")
+    end
+
+    it "routes to pending" do
+      get("/members/pending").should route_to("members#pending")
+    end
+
+    it "routes to approve" do
+      get("/members/1/approve").should route_to("members#approve", :id => "1")
+    end
+
+  end
+
+end
