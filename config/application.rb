e4b18cb8adfadaacad476ba0282336665fb03ffc
Jason Berry <jberry@cyrusinnovation.com>
2012-04-17 12:25:11 -0400

    Change setting in application.rb for assets:precompile -- Heroku again.

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module HackerUnion
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+    config.assets.initialize_on_precompile = false
+
   end
 end
