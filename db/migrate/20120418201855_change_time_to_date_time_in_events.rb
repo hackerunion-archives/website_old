d27ba6277acdbb83efcef507a927ecd53527557d
Jason Berry <jberry@cyrusinnovation.com>
2012-04-19 11:42:29 -0400

    It's harder than it seems to convert time to datetime - thankfully there's no important data

--- db/migrate/20120418201855_change_time_to_date_time_in_events.rb
+++ db/migrate/20120418201855_change_time_to_date_time_in_events.rb
@@ -1,6 +1,8 @@
 class ChangeTimeToDateTimeInEvents < ActiveRecord::Migration
   def change
-    change_column :events, :start_time, :datetime
-    change_column :events, :end_time, :datetime
+    remove_column :events, :start_time
+    remove_column :events, :end_time
+    add_column :events, :starts, :datetime
+    add_column :events, :ends, :datetime
   end
 end
--- db/migrate/20120419150735_rename_start_and_end_on_event.rb
+++
@@ -1,6 +0,0 @@
-class RenameStartAndEndOnEvent < ActiveRecord::Migration
-  def change
-    rename_column :events, :start_time, :starts
-    rename_column :events, :end_time, :ends
-  end
-end
