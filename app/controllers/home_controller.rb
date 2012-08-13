41396470ac53ba32e0b8c6ae24fa95e983c2d8b5
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-08-13 14:03:15 -0400

    fixed up ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	redirect_to '/members/sign_up' unless member_signed_in?
+  	redirect_to '/members/sign_up' #unless member_signed_in?
     @events = Event.upcoming(2)
     @announcements = Announcement.latest(5)
   end
